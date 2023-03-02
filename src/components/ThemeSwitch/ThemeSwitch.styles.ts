c088b451e902d42ffc032b9885808cbf968c56f0
Dandara Estrela <dandara_estrela@hotmail.com>
2023-03-02 15:19:15 -0300

    feat: adjusting theme switch

--- src/components/ThemeSwitch/ThemeSwitch.styles.ts
+++ src/components/ThemeSwitch/ThemeSwitch.styles.ts
@@ -4,19 +4,48 @@ interface SwitchProps {
   isThemeLight: boolean;
 }
 
-export const Wrapper = styled.div<SwitchProps>`
+export const Wrapper = styled.button<SwitchProps>`
   display: flex;
-  width: 70px;
+  width: 60px;
   height: 30px;
-  padding: 4px;
-  gap: 12px;
-  background-color: ${({ theme }) => theme.colors.inputBackground};
   position: relative;
+
+  padding: 3px 4px;
+  gap: 12px;
+
+  background-color: ${({ theme }) => theme.colors.inputBackground};
+  border-radius: 16px;
+  border: none;
+
+  cursor: pointer;
+
   :after {
     content: "";
-    position: absolute;
     height: 24px;
     width: 24px;
-    background-color: pink;
+    position: absolute;
+    left: ${({ isThemeLight }) => (isThemeLight ? "calc(50% + 2px)" : "4px")};
+
+    background-color: ${({ theme }) => theme.colors.headerBackground};
+    border-radius: 50%;
+    transition: left 0.5s;
+  }
+
+  svg path {
+    transition: 0.4s;
+  }
+
+  svg:first-child {
+    path {
+      fill: ${({ theme, isThemeLight }) =>
+        isThemeLight ? theme.colors.purple : theme.colors.inputBackground};
+    }
+  }
+
+  svg:last-child {
+    path {
+      fill: ${({ theme, isThemeLight }) =>
+        isThemeLight ? theme.colors.inputBackground : theme.colors.purple};
+    }
   }
 `;
