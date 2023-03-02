9862001476abdb6898f5776d95d9938ace30f07d
Dandara Estrela <dandara_estrela@hotmail.com>
2023-03-02 15:19:00 -0300

    styles: adjusting header styles

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
+import Logo from "@/assets/Logo.svg";
+
 export const Wrapper = styled.div`
   display: flex;
   align-items: flex-start;
   justify-content: center;
   width: 100%;
   height: 124px;
-  padding: 20px 20% 0 20%;
+  padding: 30px 20%;
   background-color: ${({ theme }) => theme.colors.headerBackground};
+  background-image: url(${Logo});
+  background-repeat: no-repeat;
+  background-position: center 20px;
+  button:last-child {
+    margin-left: auto;
+  }
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,9 @@
 import * as S from "./Header.styles";
-import Logo from "../../assets/Logo.svg";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
 
 const Header = () => {
   return (
     <S.Wrapper>
-      <img src={Logo} />
       <ThemeSwitch />
     </S.Wrapper>
   );
