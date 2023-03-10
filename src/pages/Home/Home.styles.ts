442c4fba1dcd007459309d6ad2fd5f0030c6c882
Dandara Estrela <dandara_estrela@hotmail.com>
2023-03-09 21:41:31 -0300

    feat: adding task creator to home

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -3,7 +3,22 @@ import styled from "styled-components";
 export const Wrapper = styled.main`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100vw;
   height: 100vh;
   background-color: ${({ theme: { colors } }) => colors.background};
+`;
+
+export const TaskCreatorForm = styled.form`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: -22px;
+  gap: 8px;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,40 @@
-import Header from "../../components/Header/Header";
+import { FormEvent, useState } from "react";
+
+import Button from "@/components/Button/Button";
+import TextInput from "@/components/TextInput/TextInput";
+import Header from "@/components/Header/Header";
+import { ReactComponent as Plus } from "@/assets/plus.svg";
+
 import * as S from "./Home.styles";
 
 const Home = () => {
+  const [newTaskText, setNewTaskText] = useState("");
+  const [tasksList, setTaskList] = useState<Array<string>>([]);
+
+  const onAddTask = (e: FormEvent) => {
+    e.preventDefault();
+    setTaskList((prev) => [...prev, newTaskText]);
+    setNewTaskText("");
+  };
+
   return (
     <S.Wrapper>
       <Header />
-      <p>teste</p>
+      <S.ContentWrapper>
+        <S.TaskCreatorForm onSubmit={onAddTask}>
+          <TextInput
+            placeholder="Adicione uma nova tarefa"
+            value={newTaskText}
+            onChange={setNewTaskText}
+          />
+          <Button type="submit">
+            Criar <Plus />
+          </Button>
+        </S.TaskCreatorForm>
+        {tasksList.length
+          ? tasksList.map((task) => <p>{task}</p>)
+          : "Você ainda não tem tasks cadastradas"}
+      </S.ContentWrapper>
     </S.Wrapper>
   );
 };
