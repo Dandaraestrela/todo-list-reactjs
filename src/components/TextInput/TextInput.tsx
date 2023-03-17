83382c68be2179663fd8127979c6e542527614ef
Dandara Estrela <dandara_estrela@hotmail.com>
2023-03-17 07:31:54 -0300

    feat: adding text input component

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,20 +1,26 @@
+import { InputHTMLAttributes, forwardRef, ChangeEvent } from "react";
+
 import * as S from "./TextInput.styles";
 
-interface TextInputProps {
+interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   placeholder?: string;
   value: string;
-  onChange: (value: string) => void;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextInput = ({ label, placeholder, value, onChange }: TextInputProps) => (
-  <S.Wrapper>
-    {label}
-    <S.Input
-      placeholder={placeholder}
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    />
-  </S.Wrapper>
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+  ({ label, placeholder, value, onChange, ...props }, ref) => (
+    <S.Wrapper>
+      {label}
+      <S.Input
+        ref={ref}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e)}
+        {...props}
+      />
+    </S.Wrapper>
+  )
 );
 export default TextInput;
